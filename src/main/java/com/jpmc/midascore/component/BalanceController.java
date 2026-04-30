815a2c9d68a0783ca15c76c9936de651f5340386
Psheawen <psheawen@gmail.com>
2026-04-30 18:15:36 +0800

    Completed JPMorgan Task 5 :3

---
+++ src/main/java/com/jpmc/midascore/component/BalanceController.java
@@ -0,0 +1,28 @@
+package com.jpmc.midascore.component;
+
+import com.jpmc.midascore.entity.UserRecord;
+import com.jpmc.midascore.foundation.Balance;
+import com.jpmc.midascore.repository.UserRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BalanceController {
+    final private UserRepository userRepository;
+
+    public BalanceController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/balance")
+    public Balance getBalance(@RequestParam long userId) {
+        UserRecord user = userRepository.findById(userId);
+
+        if (user == null) {
+            return new Balance(0);
+        } else {
+            return new Balance(user.getBalance());
+        }
+    }
+}
